bdcad837892eb7409a5d097bd9a742ecf53662a8
Maxim Petrovsky <max@iMac-Max.lan>
2016-05-13 17:44:08 +0300

    added init with style to uiimageview.

--- RMStyleManager/Classes/Components/RMStylishImageView.h
+++ RMStyleManager/Classes/Components/RMStylishImageView.h
@@ -1,3 +1,4 @@
+
 //
 //  RMStylishImageView.h
 //  Pods
@@ -9,6 +10,16 @@
 #import <UIKit/UIKit.h>
 
 @interface RMStylishImageView : UIImageView
+
+- (instancetype)initWithFrame:(CGRect)frame
+                        style:(NSString *)style;
+
+- (instancetype)initWithImage:(UIImage *)image
+                        style:(NSString *)style;
+
+- (instancetype)initWithImage:(UIImage *)image
+             highlightedImage:(UIImage *)highlightedImage
+                        style:(NSString *)style;
 
 @property(nonatomic, strong) IBInspectable NSString *style;
 
